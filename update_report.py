ad48f1f3f3a7d51c2ea427d7f1e10bf95439a7fa
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-01-31 13:38:27 +0330

    Update sheet ids

--- update_report.py
+++ update_report.py
@@ -22,18 +22,18 @@ os.system('clear')
 update_type = int(input("Please choose your update type \n 1.Complete \n 2.Fast \n"))
 
 if (update_type == 1):
-    # print("... Connecting FC ...")
-    # #Update FC
-    # jira_connector = workflow.connect_jira(username, password)
-    # print("Jira Connected")
+    print("... Connecting FC ...")
+    #Update FC
+    jira_connector = workflow.connect_jira(username, password)
+    print("Jira Connected")
 
-    # sheet_connector = workflow.connect_sheet("[FC] Sprints - 01", fc_id)
-    # print("G-Sheet Connected")
-    # print("-----------------------------------------------")
+    sheet_connector = workflow.connect_sheet("[FC] Sprints - 01", fc_id)
+    print("G-Sheet Connected")
+    print("-----------------------------------------------")
 
-    # print("Start Updating FC ...")
-    # workflow.update_tickets(jira_connector, sheet_connector, "key", "summary", "epic", "fc_area","developed_by", "estimate", "review_by", "review_estimate", "impact", "status")
-    # print("FC Updated ^^")
+    print("Start Updating FC ...")
+    workflow.update_tickets(jira_connector, sheet_connector, "key", "summary", "epic", "fc_area","developed_by", "estimate", "review_by", "review_estimate", "impact", "status")
+    print("FC Updated ^^")
 
     print("-----------------------------------------------")
     print("... Connecting DEL ...")
