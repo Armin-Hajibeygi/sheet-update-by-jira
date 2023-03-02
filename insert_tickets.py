8470a528ed10fde3ef19e4f91a8c94990b2d578f
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-03-03 02:04:45 +0330

    feat: make read from csv better

--- insert_tickets.py
+++ insert_tickets.py
@@ -23,43 +23,30 @@ df.loc[squad_dict[squad], 'sheet_id'] = sheet_id
 # Save the updated DataFrame to the CSV file
 df.to_csv("sheet_id.csv", index=False)
 
-file = open('sheet_id.csv')
-csvreader = csv.reader(file)
-sheet_ids = dict()
-for row in csvreader:
-    sheet_ids[row[0]] = row[1]
-
-fc_id = int(sheet_ids["FC"])
-del_id = int(sheet_ids["DEL"])
-front_id = int(sheet_ids["Front"])
-platform_id = int(sheet_ids["Plat"])
-inventory_id = int(sheet_ids["Inventory"])
-
-file.close()
+# Load the sheet IDs from the CSV file into a dictionary
+with open('sheet_id.csv') as f:
+    sheet_ids = dict(csv.reader(f))
 
 jira_connector = workflow.connect_jira(username, password)
 print("Jira Connected")
 
+# Connect to Google Sheets and fetch data from Jira
 if squad == 1:
-    sheet_connector = workflow.connect_sheet("[FC] Sprints - 01", fc_id)
+    sheet_connector = workflow.connect_sheet("[FC] Sprints - 01", int(sheet_ids["FC"]))
     jql = 'project = DKFC AND Sprint in openSprints() AND  (status = "Sprint Backlog" OR status = In-Progress) AND Side = Back-End ORDER BY priority DESC, cf[10201] ASC'
     workflow.get_ticket(jira_connector, sheet_connector, jql, "key", "summary", "epic","fc_area", "developed_by", "estimate", "review_by", "review_estimate", "impact", "status")
 elif squad == 2:
-    sheet_connector = workflow.connect_sheet("[DEL] Sprints - All", del_id)
+    sheet_connector = workflow.connect_sheet("[DEL] Sprints - All", int(sheet_ids["DEL"]))
     print("G-Sheet Connected")
     jql = 'project = LG AND Sprint in openSprints() AND  (status = "Sprint Backlog" OR status = In-Progress) AND Side = Back-End ORDER BY priority DESC, cf[10201] ASC'
     workflow.get_ticket(jira_connector, sheet_connector, jql, "key", "summary", "epic", "del_area", "developed_by", "estimate", "unit_test_estimate", "review_by", "impact", "status") 
 elif squad == 3:
-    sheet_connector = workflow.connect_sheet("[OPS] Front Sprints - 01", front_id)
+    sheet_connector = workflow.connect_sheet("[OPS] Front Sprints - 01", int(sheet_ids["Front"]))
     jql = '(project = DKFC OR project = Delivery) AND  Sprint in openSprints() AND  (status = "Sprint Backlog" OR status = In-Progress) AND Side = Front-End ORDER BY priority DESC, cf[10201] ASC'
     workflow.get_ticket(jira_connector, sheet_connector, jql, "key", "summary", "side", "epic", "developed_by", "estimate", "review_by", "review_estimate", "impact", "status") 
 elif squad == 4:
-    sheet_connector = workflow.connect_sheet("[Platform] Sprints", platform_id)
+    sheet_connector = workflow.connect_sheet("[Platform] Sprints", int(sheet_ids["Plat"]))
     jql = 'project =Platform AND status in ("DX Sprint Backlog", "SRE Sprint Backlog", "SN Sprint Backlog", "Infrastructure Sprint Backlog") AND Sprint in openSprints() ORDER BY priority DESC, cf[10201] ASC'
-    workflow.get_ticket(jira_connector, sheet_connector, jql, "key", "summary", "step", "assignee","epic", "estimate", "impact", "status") 
-elif squad == 5:
-    sheet_connector = workflow.connect_sheet(" Inventory", inventory_id)
-    jql = 'project = DKFC AND Sprint in openSprints() AND  (status = "Sprint Backlog" OR status = In-Progress) AND Side = Back-End ORDER BY priority DESC, cf[10201] ASC'
-    workflow.get_ticket(jira_connector, sheet_connector, jql, "key", "summary", "epic","fc_area", "developed_by", "estimate", "review_by", "review_estimate", "impact", "status")
-
+    workflow.get_ticket(jira_connector, sheet_connector)
+                        
 os.system('clear')
