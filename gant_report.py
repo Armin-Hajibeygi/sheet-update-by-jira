b53ebd420278841d0cbe4770b66a4dbd4cdf6d0a
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-04-17 17:24:25 +0330

    style: clean code

--- gant_report.py
+++ gant_report.py
@@ -8,7 +8,8 @@ from datetime import datetime
 username = const.USERNAME
 password = const.PASSWORD
 
-jira = JIRA (basic_auth=(username, password), options={'server':'https://dkjira.digikala.com'})
+jira = JIRA(basic_auth=(username, password), options={
+            'server': 'https://dkjira.digikala.com'})
 jql = "key = DKFC-12289"
 
 os.system('clear')
@@ -16,12 +17,13 @@ os.system('clear')
 project = pd.DataFrame(columns=['issue', 'date', 'action_by', 'from', 'to'])
 
 for issue in jira.search_issues(jql, maxResults=500):
-    changelog = jira.issue(str(issue.key), expand='changelog').changelog    
+    changelog = jira.issue(str(issue.key), expand='changelog').changelog
 
-    for history in changelog.histories:        
-        for item in history.items:            
+    for history in changelog.histories:
+        for item in history.items:
             if item.field == 'status':
-                action = {'issue': str(issue.key), 'date': datetime.strptime(history.created[:10], '%Y-%M-%d').date(), 'action_by': history.author, 'from': item.fromString, 'to': item.toString}
+                action = {'issue': str(issue.key), 'date': datetime.strptime(
+                    history.created[:10], '%Y-%M-%d').date(), 'action_by': history.author, 'from': item.fromString, 'to': item.toString}
                 project = project.append(action, ignore_index=True)
 
 os.system('clear')
