04ba33c970e0f0c97cc133b819ff501837a0e2bc
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-04-22 14:33:00 +0330

    feat: v1.0

--- gant_report.py
+++ gant_report.py
@@ -1,56 +1,70 @@
+import pandas as pd
+from datetime import datetime
 from jira import JIRA
+import plotly.express as px
 import os
 import const
-from datetime import datetime
-import plotly.express as px
-import pandas as pd
 
-username = const.USERNAME
+server_url = const.SERVER
 password = const.PASSWORD
+username = const.USERNAME
+project_epic = 'LG-3093'
 
 jira = JIRA(basic_auth=(username, password), options={
-                          'server': const.SERVER})
-
-project_epic = 'DKFC-11516'
-jql = '"Epic Link" = ' + project_epic
+    'server': server_url})
 
 epic_jql = "key = " + project_epic
 epic = jira.search_issues(epic_jql)
 epic_name = epic[0].fields.customfield_10104
 
-project = pd.DataFrame(
-    columns=['issue', 'start', 'action_by', 'from', 'status'])
+jql = f'"Epic Link" = {project_epic}'
+issues = jira.search_issues(jql, maxResults=500)
 
-for issue in jira.search_issues(jql, maxResults=500):
+actions = []
+for issue in issues:
     changelog = jira.issue(str(issue.key), expand='changelog').changelog
-    action = {'issue': str(issue.key), 'start': datetime.strptime(
-        str(issue.fields.created)[:19], '%Y-%m-%dT%H:%M:%S'), 'action_by': issue.fields.reporter.displayName, 'from': "", 'status': "Analysis Backlog"}
-    project = project.append(action, ignore_index=True)
+
+    action = {'issue': str(issue.key),
+              'start': datetime.strptime(str(issue.fields.created)[:19], '%Y-%m-%dT%H:%M:%S'),
+              'action_by': issue.fields.reporter.displayName,
+              'from': "",
+              'status': "Analysis Backlog"}
+    actions.append(action)
 
     for history in changelog.histories:
         for item in history.items:
             if item.field == 'status':
-                action = {'issue': str(issue.key), 'start': datetime.strptime(
-                    history.created[:19], '%Y-%m-%dT%H:%M:%S'), 'action_by': history.author, 'from': item.fromString, 'status': item.toString}
-                project = project.append(action, ignore_index=True)
+                action = {'issue': str(issue.key),
+                          'start': datetime.strptime(history.created[:19], '%Y-%m-%dT%H:%M:%S'),
+                          'action_by': history.author.displayName,
+                          'from': item.fromString,
+                          'status': item.toString}
+                actions.append(action)
 
-    if ((str(issue.fields.status) != 'Done') and (str(issue.fields.status) != 'Invalid')):
-        action = {'issue': str(issue.key), 'start': datetime.now(
-        ), 'action_by': issue.fields.reporter.displayName, 'from': "", 'status': str(issue.fields.status)}
-        project = project.append(action, ignore_index=True)
+    if str(issue.fields.status) not in ['Done', 'Invalid']:
+        action = {'issue': str(issue.key),
+                  'start': datetime.now(),
+                  'action_by': issue.fields.reporter.displayName,
+                  'from': "",
+                  'status': str(issue.fields.status)}
+        actions.append(action)
 
-os.system('clear')
-project.sort_values(by=['issue', 'start']).reset_index(drop=True)
+df = pd.DataFrame(actions)
 
-for i in range(0, len(project)-1):
-    if (project.loc[i, 'issue'] == project.loc[i+1, 'issue']):
-        project.loc[i, 'end'] = project.loc[i+1, 'start']
+#df['start'] = pd.to_datetime(df['start'], utc=True)
+df = df.sort_values(by=['issue', 'start'], ascending=[False, True]).reset_index(drop=True)
 
-fig = px.timeline(project, x_start='start', x_end='end', y='issue', color='status', title=(
-    epic_name + ' - Total Number of Tickets:' + str(len(project['issue'].unique()))))
+for i in range(0, len(df)-1):
+    if (df.loc[i, 'issue'] == df.loc[i+1, 'issue']):
+        df.loc[i, 'end'] = df.loc[i+1, 'start']
 
-# fig.update_yaxes(visible=False, showticklabels=False)
+fig = px.timeline(df, x_start='start', x_end='end', y='issue', color='status', title=(
+    epic_name + ' - Total Number of Tickets:' + str(len(df['issue'].unique()))))
 
 epic_name = epic_name.replace(' ', '-')
 fig_address = epic_name + '.html'
-fig.write_html('reports/' + fig_address, auto_open=True)
+report_dir = 'reports/'
+if not os.path.exists(report_dir):
+    os.mkdir(report_dir)
+
+fig.write_html(os.path.join(report_dir, fig_address), auto_open=True)
