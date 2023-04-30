4035b0da90cecf03a7ca6aa1eab2e1d06445d98e
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-04-30 16:25:08 +0330

    feat: update version of python

--- complete_update.py
+++ complete_update.py
@@ -1,4 +1,4 @@
 import subprocess
 
-subprocess.run(["python", "update_report.py"])
-subprocess.run(["python", "del_metrics.py"])
+subprocess.run(["python3", "update_report.py"])
+subprocess.run(["python3", "del_metrics.py"])
