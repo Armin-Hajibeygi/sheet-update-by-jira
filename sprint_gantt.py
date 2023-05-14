0c7007cffc082149b3c1d389d5a9517f28a76bcb
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-05-14 15:17:04 +0330

    style: add comments to sprint_gantt.py

--- sprint_gantt.py
+++ sprint_gantt.py
@@ -7,18 +7,24 @@ from datetime import datetime
 from jira import JIRA
 from gant_report import create_gantt
 
+# Define Consts
 server_url = const.SERVER
 password = const.PASSWORD
 username = const.USERNAME
+del_board_id = 12
+fc_board_id = 11
 
+# Get sheet ids
 with open('sheet_id.csv') as f:
     sheet_ids = dict(csv.reader(f))
 
 os.system('clear')
 
+# Get squad
 squad = int(
         input("Choose Squad: \n 1.FC \n 2.DEL \n"))
 
+# Connect jira
 jira = JIRA(basic_auth=(username, password), options={
     'server': server_url})
 
@@ -27,6 +33,7 @@ sheet_connector = googleSheet.Sheet("[FC] Sprints - 02", int(sheet_ids["FC"]))
 tickets, number_of_tickets = sheet_connector.get_sheet_tickets()
 sheet_name = sheet_connector.get_sheet_name()
 
+# Get tickets
 issues = list()
 outbound_issues = list()
 inbound_issues = list()
@@ -48,11 +55,10 @@ for ticket in tickets:
 
 os.system('clear')
 
+# Handle time limitation
 time_limitation = int(
     input("Choose Time Limitation: \n 1.2 Weeks \n 2.All Time \n"))
 if time_limitation == 1:
-    del_board_id = 12
-    fc_board_id = 11
     del_sprint_id = jira.sprints(del_board_id)[-1].id
     fc_sprint_id = jira.sprints(fc_board_id)[-1].id
 
@@ -68,6 +74,7 @@ else:
     start_date = datetime.min
     gantt_title = sheet_name + ' - All Time Report'
 
+# Create gantt charts
 if squad == 1:
     create_gantt(inbound_issues, gantt_title + ' - Inbound', start_date)
     create_gantt(outbound_issues, gantt_title + ' - Outbound', start_date)
