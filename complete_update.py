39d0d7bbbd8ebeca8ad702b4adddc2ab67e59d44
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-05-20 10:51:32 +0330

    feat: add logs

--- complete_update.py
+++ complete_update.py
@@ -1,4 +1,20 @@
+#!/usr/bin/env python3
+
 import subprocess
+import logging
+import datetime
+
+# Set up logging
+logging.basicConfig(filename='logs.log', level=logging.DEBUG)
+
+# Create a formatter that includes a timestamp
+formatter = logging.Formatter('%(asctime)s: %(message)s')
+
+# Set the formatter for the root logger
+logging.getLogger().handlers[0].setFormatter(formatter)
+
+# Log some messages
+logging.info('This is an info message')
 
 subprocess.run(["python3", "update_report.py"], check=True)
 subprocess.run(["python3", "del_metrics.py"], check=True)
