3908889334d14a93c1ae3972f08240014b4055dd
Armin Hajibeygi <armin.beygi2000@gmail.com>
2023-05-22 21:49:37 +0330

    feat: Extract insert_issue method

--- JiraSheetBridge.py
+++ JiraSheetBridge.py
@@ -33,32 +33,23 @@ class Connector:
 
     def update_report(self) -> None:
         self.get_issues_from_sheet()
-
         for index, ticket in enumerate(self.sheet_issues):
             jql = "key = " + ticket
-
             self.remaining_issues -= 1
             print(f"Remaining: {self.remaining_issues}")
-
             for issue in self.jira_connector.search_issues(jql):
-                self.issue_details = list()
-                for field_name in self.fields:
-                    field_class = Connector.class_map.get(field_name)
-                    self.issue_details.append(field_class(issue, self.jira_connector).get_field())
-                self.insert_issues(index, 0)
+                self.insert_issue(issue, index, 0)
 
     def create_report(self) -> None:
         issues = self.jira_connector.search_issues(self.jql, maxResults=500)
         print(f"Number of tickets: {len(issues)}")
-
         for index, issue in enumerate(issues):
             print(f"Remaining: {len(issues) - index}")
+            self.insert_issue(issue, index, 0)
 
-            self.issue_details = list()
-            for field_name in self.fields:
-                field_class = Connector.class_map.get(field_name)
-                self.issue_details.append(field_class(issue, self.jira_connector).get_field())
-            self.insert_issues(index, 0)
-
-    def insert_issues(self, index: int, skip: int) -> None:
+    def insert_issue(self, issue, index: int, skip: int) -> None:
+        self.issue_details = list()
+        for field_name in self.fields:
+            field_class = Connector.class_map.get(field_name)
+            self.issue_details.append(field_class(issue, self.jira_connector).get_field())
         self.google_sheet_connector.insert_ticket(self.issue_details, skip, index + 2)
